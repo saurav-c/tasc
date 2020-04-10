3ca6fbc80d6a67e333b49a4d38509b8fe9917281
saurav_c <sauravc@berkeley.edu>
2020-04-09 17:05:13 -0700

    Created AftSI Server struct and GRPC service code

--- cmd/aftsi/main.go
+++ cmd/aftsi/main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"net"
 	"fmt"
+	"context"
 	"log"
 
-	pb "github.com/saurav-c/aftsi/proto/aftsi"
-
-	"github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/saurav-c/aftsi/proto"
 	"google.golang.org/grpc"
+	"github.com/golang/protobuf/ptypes/empty"
 )
 
 const (
 	port = ":7654"
 )
+
+type TransactionMngrEntry struct {
+
+}
+
+func (s *AftSIServer) StartTransaction(context.Context, *empty.Empty) (*pb.TransactionID, error) {
+	panic("implement me")
+}
+
+func (s *AftSIServer) Read(context.Context, *pb.ReadRequest) (*pb.TransactionResponse, error) {
+	panic("implement me")
+}
+
+func (s *AftSIServer) Write(context.Context, *pb.WriteRequest) (*pb.TransactionResponse, error) {
+	panic("implement me")
+}
+
+func (s *AftSIServer) CommitTransaction(context.Context, *pb.TransactionID) (*pb.TransactionResponse, error) {
+	panic("implement me")
+}
+
+func (s *AftSIServer) AbortTransaction(context.Context, *pb.TransactionID) (*pb.TransactionResponse, error) {
+	panic("implement me")
+}
+
 
 func main() {
 	lis, err := net.Listen("tcp", port)
@@ -21,6 +46,9 @@ func main() {
 		log.Fatal("Could not start server on port %s: %v\n", port, err)
 	}
 
+	// TODO: Figure out actual AFTSI server setup calls
 	server := grpc.NewServer()
+	aftsi, config := NewAftSIServer()
+	pb.RegisterAftSIServer(server, aftsi)
 
 }
--- cmd/aftsi/server.go
+++ cmd/aftsi/server.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+  "sync"
+  "time"
+)
+
+type TransactionEntry struct {
+  begin_ts      string
+  end_ts        string
+  readSet       []string
+  coWrittenSets [][]string
+  status        int8
+  opCounter     int64
+}
+
+type AftSIServer struct {
+	IpAddress            string
+	StorageManager       storage.StorageManager
+	ConsistencyManager   consistency.ConsistencyManager
+	TransactionTable     map[string]*TransactionEntry
+	TransactionTableLock *sync.RWMutex
+	WriteBuffer          map[string][]*keyUpdate
+	WriteBufferLock      *sync.RWMutex
+	ReadCache            map[string]pb.KeyValuePair
+	ReadCacheLock        *sync.RWMutex
+}
+
+func NewAftSIServer() {
+  panic("implement me!")
+}
