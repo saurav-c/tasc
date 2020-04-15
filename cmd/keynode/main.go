5d82791e76cd66da41f23dc84409d8db1dae38a8
Taj Shaik <tajshaik24@gmail.com>
2020-04-15 00:45:30 -0700

    Finished all core keyNode except readKey

--- cmd/keynode/main.go
+++ cmd/keynode/main.go
@@ -5,25 +5,93 @@ const (
 	TRANSACTION_FAILURE = 1;
 )
 
+func (k *KeyNode) _deleteFromPendingKVI (keys []string, keyEntry *keyVersion, action int8) {
+	for _, key := range keys {
+		pLock := k.pendingKeyVersionIndexLock[key]
+		pLock.RLock()
+		pendingKeyVersions := k.pendingKeyVersionIndex[key]
+		pLock.RUnlock()
+
+		indexEntry := FindIndex(pendingKeyVersions, keyEntry)
+		newPendingKeyVersions := append(pendingKeyVersions[:indexEntry], pendingKeyVersions[indexEntry+1:]...)
+		pLock.Lock()
+		k.pendingKeyVersionIndex[key] = newPendingKeyVersions
+		pLock.Unlock()
+
+		if action == TRANSACTION_SUCCESS {
+			lock := k.keyVersionIndexLock[key]
+			lock.RLock()
+			committedKeyVersions := k.keyVersionIndex[key]
+			lock.RUnlock()
+
+			keyVersion := pendingKeyVersions[indexEntry]
+			newCommittedKeyVersions := InsertParticularIndex(committedKeyVersions, keyVersion)
+
+			lock.Lock()
+			k.keyVersionIndex[key] = newCommittedKeyVersions
+			lock.Unlock()
+		}
+	}
+}
+
 func (k *KeyNode) readKey (tid string, key string, readList []string, begints string, lowerBound string) (txnid string, keyVersion string, value []byte, coWritten []string) {
 
 	return tid, "", nil, nil
 }
 
-func (k *KeyNode) validate (tid string, txnBeginTS string, txnCommitTS string, keys []string, writeSet []string) (txnid string, action int, writeBuffer map[string][]byte) {
+func (k *KeyNode) validate (tid string, txnBeginTS string, txnCommitTS string, keys []string) (txnid string, action int, writeBuffer map[string][]byte) {
 	for _, key := range keys {
 		pendingKeyVersions := k.pendingKeyVersionIndex[key]
 		for _, keyVersion := range pendingKeyVersions {
 			keyCommitTS := keyVersion.CommitTS
 			if txnBeginTS < keyCommitTS && keyCommitTS < txnCommitTS {
-				return tid, 1, nil
+				return tid, TRANSACTION_FAILURE, nil
 			}
 		}
 	}
-
-	return tid, 0, nil
+	NewKeyVersion := keyVersion{tid: tid, CommitTS: txnCommitTS}
+	for _, key := range keys {
+		lock := k.pendingKeyVersionIndexLock[key]
+		lock.RLock()
+		pendingKeyVersions := k.pendingKeyVersionIndex[key]
+		lock.RUnlock()
+		updatedKeyVersions := InsertParticularIndex(pendingKeyVersions, &NewKeyVersion)
+		lock.Lock()
+		k.pendingKeyVersionIndex[key] = updatedKeyVersions
+		lock.Unlock()
+	}
+	k.pendingTxnCache[tid] = &pendingTxn{
+		keys:     keys,
+		commitTS: txnCommitTS,
+	}
+	return tid, TRANSACTION_SUCCESS, nil
 }
 
-func (k *KeyNode) endTransaction (txnid string, action int, writeBuffer map[string][]byte) {
-
+func (k *KeyNode) endTransaction (tid string, action int, writeBuffer map[string][]byte) {
+	pendingTxn, ok := k.pendingTxnCache[tid]
+	if !ok {
+		return
+	}
+	delete(k.pendingTxnCache, tid)
+	TxnKeys := pendingTxn.keys
+	CommitTS := pendingTxn.commitTS
+	keyEntry := &keyVersion{
+		tid:      tid,
+		CommitTS: CommitTS,
+	}
+	if action == TRANSACTION_FAILURE {
+		// Deleting the entries from the Pending Key-Version Index
+		k._deleteFromPendingKVI(TxnKeys, keyEntry, TRANSACTION_FAILURE)
+		return
+	}
+	var writeSet []string
+	for key := range writeBuffer {
+		writeSet = append(writeSet, key)
+	}
+	// Deleting the entries from the Pending Key-Version Index and storing in Committed Txn Cache
+	k._deleteFromPendingKVI(TxnKeys, keyEntry, TRANSACTION_SUCCESS)
+	committedTxnLock := k.commitedTxnCacheLock[tid]
+	committedTxnLock.Lock()
+	defer committedTxnLock.Unlock()
+	k.committedTxnCache[tid] = writeSet
 }
--- cmd/keynode/server.go
+++ cmd/keynode/server.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 func InsertParticularIndex(list []*keyVersion, kv *keyVersion) []*keyVersion {
 	if len(list) == 0 {
 		return []*keyVersion{kv}
@@ -34,13 +36,33 @@ type keyVersion struct {
 	CommitTS string
 }
 
-type KeyNode struct {
-	keyVersionIndex        map[string][]*keyVersion
-	pendingKeyVersionIndex map[string][]*keyVersion
-	committedTxnCache      map[string][]string
-	readCache              map[string]byte
+type pendingTxn struct {
+	keys []string
+	commitTS string
 }
 
-func NewKeyNode(KeyNodeIP string) {
-	panic("implement me")
+type KeyNode struct {
+	keyVersionIndex             map[string][]*keyVersion
+	keyVersionIndexLock         map[string]*sync.RWMutex
+	pendingKeyVersionIndex      map[string][]*keyVersion
+	pendingKeyVersionIndexLock  map[string]*sync.RWMutex
+	pendingTxnCache             map[string]*pendingTxn
+	committedTxnCache           map[string][]string
+	commitedTxnCacheLock        map[string]*sync.RWMutex
+	readCache                   map[string][]byte
+	readCacheLock               *sync.RWMutex
+}
+
+func NewKeyNode(KeyNodeIP string) (*KeyNode, int, error){
+	// TODO: Need to create ZMQ Connections
+	return &KeyNode{
+		keyVersionIndex:           	make(map[string][]*keyVersion),
+		keyVersionIndexLock:        make(map[string]*sync.RWMutex),
+		pendingKeyVersionIndex:     make(map[string][]*keyVersion),
+		pendingKeyVersionIndexLock: make(map[string]*sync.RWMutex),
+		committedTxnCache:          make(map[string][]string),
+		commitedTxnCacheLock:       make(map[string]*sync.RWMutex),
+		readCache:                  make(map[string][]byte),
+		readCacheLock:              &sync.RWMutex{},
+	}, 0, nil
 }
