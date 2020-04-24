20dfadb59b58e9bf9d465141c94314390772a1d0
Taj Shaik <tajshaik24@gmail.com>
2020-04-23 17:57:40 -0700

    setup.sh completed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
+# Creating the Directories and Defining ENVVARS
 export GOPATH=~/go
-echo "export GOPATH=~/go" >> ~/.bashrc
 mkdir $GOPATH
 mkdir $GOPATH/bin
 mkdir $GOPATH/src
 mkdir $GOPATH/pkg
-echo "PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
 export PATH=$PATH:$GOPATH/bin
+export GOBIN=$GOPATH/bin
+
+# Persist Information in ~/.bashrc
+echo "export GOPATH=~/go" >> ~/.bashrc
+echo "PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+echo "export GOBIN=$GOPATH/bin" >> ~/.bashrc
+
+# Installing the dependencies
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:longsleep/golang-backports
@@ -13,16 +20,55 @@ sudo apt-get update
 sudo apt-get install -y golang-go wget unzip git ca-certificates net-tools python3-pip libzmq3-dev curl apt-transport-https
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.10.0/protoc-3.10.0-linux-x86_64.zip
 sudo unzip protoc-3.10.0-linux-x86_64.zip -d /usr/local
-mkdir -p $GOPATH/src/github.com/saurav-c
-cd $GOPATH/src/github.com/saurav-c
-sudo git clone https://github.com/saurav-c/aftsi
-cd afsti
 sudo go get -u google.golang.org/grpc
 sudo go get -u github.com/golang/protobuf/protoc-gen-go
+sudo go get -u github.com/pebbe/zmq4
+sudo go get -u github.com/aws/aws-sdk-go
+sudo go get -u github.com/go-redis/redis
 
-cd proto/aftsi
-sudo protoc -I aftsi/ aftsi/aftsi.proto --go_out=plugins=grpc:aftsi
-cd $GOPATH/src/github.com/saurav-c/aftsi/cmd/aftsi
-sudo go build
-sudo ./aftsi
+# Making the directory
+sudo mkdir -p $GOPATH/src/github.com/saurav-c
+cd $GOPATH/src/github.com/saurav-c
+
+# Cloning GitHub repo
+sudo git clone https://github.com/saurav-c/aftsi
+
+# Configuring based on node desired
+cd aftsi/proto/
+if [[ "$1" = "aftsi" ]]
+then
+  sudo protoc -I aftsi/ aftsi/aftsi.proto --go_out=plugins=grpc:aftsi
+  sudo mkdir -p aftsi/api
+  sudo mv aftsi/aftsi.pb.go aftsi/api
+
+  # Creating the executable for AFTSI
+  cd $GOPATH/src/github.com/saurav-c/aftsi/cmd/aftsi
+  sudo go build
+  ./aftsi $2 $3 $4
+fi
+
+if [[ "$1" = "keynode" ]]
+then
+  sudo protoc -I keynode/ keynode/keynode.proto --go_out=plugins=grpc:keynode
+  sudo mkdir -p keynode/api
+  sudo mv keynode/keynode.pb.go keynode/api
+
+  # Creating the executable for AFTSI
+  cd $GOPATH/src/github.com/saurav-c/aftsi/cmd/keynode
+  sudo go build
+  ./keynode
+fi
+
+if [[ "$1" = "cli" ]]
+then
+  # Creating the executable for AFTSI
+  cd $GOPATH/src/github.com/saurav-c/aftsi/cli
+  sudo go build
+  ./cli $2
+fi
+
+
+
+
+
 
