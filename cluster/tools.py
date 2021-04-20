09b23e3ddcc3aea4d20ec995c22897467e59035e
saurav_c <sauravc@berkeley.edu>
2021-04-20 16:20:55 -0700

    Created tools utility file

---
+++ cluster/tools.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from add_nodes import add_nodes
+import util
+
+NODE_TYPES = ['tasc', 'keynode', 'routing', 'lb', 'worker']
+
+def main():
+    args = sys.argv
+
+if __name__ == '__main__':
+    main()
